0116f14199ffbc042e7cf5c074a61db5d4925da7
m5preusser <124309201+m5preusser@users.noreply.github.com>
2024-03-22 10:32:53 -0600

    Update Logic.py

--- Logic.py
+++ Logic.py
@@ -1,10 +1,15 @@
 #Logic.py
-from machine import Pin
+from time import sleep
+from ili9341 import Display, color565
+from machine import Pin, SPI
  
 button_yes = None
 button_no = None
 button_off = None
 path = None
+spi = SPI(1, baudrate=40000000, sck=Pin(14), mosi=Pin(15))
+display = Display(spi, dc=Pin(6), cs=Pin(17), rst=Pin(7))
+temperature = ""
 
 def main(self):
     
@@ -33,11 +38,13 @@ def button_input():
     return response
 
 def display_text(text):
-    pass
+    display.clear()
+    display.draw_text(0,0,text)
+
 
 def question_1():
 
-    question = ""
+    question = "Nice to talk to you again! Are you feeling well today?"
     display_text(question)
     response = button_input()
 
@@ -53,7 +60,7 @@ def question_1():
 def question_2():
 
     if path == "0":
-        question = ""
+        question = "Glad you're feeling alright! I've been working on some jokes, would you be interested in hearing one?"
         display_text(question)
         response = button_input()
 
@@ -67,7 +74,7 @@ def question_2():
             pass
     
     else:
-        question = ""
+        question = "I'm sorry to hear that today's not going great. I'd like to help, are you in any physical pain?"
         display_text(question)
         response = button_input()
 
@@ -112,15 +119,23 @@ def question_3():
             pass
 
     elif path == "10":
-        question = ""
+        question = "Would you describe your pain as a headache?"
         display_text(question)
         response = button_input()
 
         if response == "yes":
-            terminating_statement = ""
-            display_text(terminating_statement)
+            question = "The current ambient temperature is " + temperature + " °C. Would you like to cool off?"
+            display_text(question)
+            response = button_input()
+
+            if response == "yes":
+                #turn on fan
+                pass
+            else:
+                terminating_statement = "I would recommend having something to eat and drinking some water. Carbohydrates such as toast or bananas are best for digestion. If your headache persists, I would suggest you call a nurse for help"
+
         elif response == "no":
-            terminating_statement = ""
+            question = ""
             display_text(terminating_statement)
         else:
             pass
