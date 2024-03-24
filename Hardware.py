99063e60c6daca32bb4db46fda77fdaf631b07df
Connor Lambie <connor.lambie@gmail.com>
2024-03-23 23:57:19 -0600

    got screen working

--- Hardware.py
+++ Hardware.py
@@ -1,2 +1,39 @@
+from machine import Pin
+from time import sleep
+from ili9341 import Display, color565
+from machine import Pin, SPI
+from xglcd_font import XglcdFont
+
+# Buttons
+yes_button = Pin(0, Pin.IN, Pin.PULL_UP)
+no_button = Pin(1, Pin.IN, Pin.PULL_UP)
+off_button = Pin(2, Pin.IN, Pin.PULL_UP)
+
+# Screen
+spi = SPI(1, baudrate=40000000, sck=Pin(14), mosi=Pin(15))
+display = Display(spi, dc=Pin(6), cs=Pin(17), rst=Pin(7))
+broadway = XglcdFont('Broadway17x15.c', 17, 15)
+
 def button_input():
-    pass
+    if yes_button.value() == 0:
+        print('Yes Button Pressed')
+        sleep(0.1)
+        return 'yes'
+
+    elif no_button.value() == 0:
+        print('No Button pressed')
+        sleep(0.1)
+        return 'no'
+
+    elif off_button.value() == 0:
+        print('off button pressed')
+        sleep(0.1)
+        return 'off'
+
+    else:
+        return ''
+
+def display_text(text: str):
+    display.clear(color565(255, 255, 255))
+    display.draw_text(0, 255, text, broadway, 0, background=color565(255, 255, 255), landscape=True)
+
--- main.py
+++ main.py
@@ -1,26 +1,17 @@
 from machine import Pin
 import time
+import Hardware
 
-yes_button = Pin(0, Pin.IN, Pin.PULL_UP)
-no_button = Pin(1, Pin.IN, Pin.PULL_UP)
-off_button = Pin(2, Pin.IN, Pin.PULL_UP)
-led1 = Pin("LED", Pin.OUT)
 
 
 def main():
     print('Main started')
+    
+    Hardware.display_text('hello world')
 
     while True:
-        if yes_button.value() == 0:
-            print('Yes Button Pressed')
-        if no_button.value() == 0:
-            print('No Button pressed')
-        if off_button.value() == 0:
-            print('off button pressed')
-
-        led1.value(True)
-        time.sleep(1)
-        led1.value(False)
+        Hardware.display_text(Hardware.button_input())
+        time.sleep(0.1)
 
 
 if __name__ == '__main__':
