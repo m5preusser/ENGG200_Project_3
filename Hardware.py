29a0b400d2795f59a2885c3dcf45fb13f5b655c8
Connor Lambie <connor.lambie@gmail.com>
2024-03-23 22:49:48 -0600

    added files

---
+++ Hardware.py
@@ -0,0 +1,2 @@
+def button_input():
+    pass
--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 from machine import Pin
 import time
 
+yes_button = Pin(0, Pin.IN, Pin.PULL_UP)
+no_button = Pin(1, Pin.IN, Pin.PULL_UP)
+off_button = Pin(2, Pin.IN, Pin.PULL_UP)
+led1 = Pin("LED", Pin.OUT)
+
 
 def main():
     print('Main started')
+
+    while True:
+        if yes_button.value() == 0:
+            print('Yes Button Pressed')
+        if no_button.value() == 0:
+            print('No Button pressed')
+        if off_button.value() == 0:
+            print('off button pressed')
+
+        led1.value(True)
+        time.sleep(1)
+        led1.value(False)
 
 
 if __name__ == '__main__':
