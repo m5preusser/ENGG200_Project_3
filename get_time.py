e6ebe28795d085e04bdafac4513620061b210d66
Connor Lambie <connor.lambie@gmail.com>
2024-03-24 22:29:21 -0600

    clock finished

--- get_time.py
+++ get_time.py
@@ -3,6 +3,10 @@ import time
 
 def set_time():
     data = urequests.get('https://timeapi.io/api/Time/current/zone?timeZone=America/Edmonton')
+    global hour_offset
+    hour_offset = time.localtime()[3]
+    global minute_offset
+    minute_offset = time.localtime()[4]
     for key, value in data.json().items():
         if key == "hour":
             global set_hour
@@ -11,13 +15,10 @@ def set_time():
         if key == "minute":
             global set_minute
             set_minute = value
-    global hour_offset
-    hour_offset = time.localtime()[3]
-    global minute_offset
-    minute_offset = time.localtime()[4]
+
 
 def hour():
-    return (time.localtime()[3] + set_hour - hour_offset + (set_minute - minute_offset) // 60) % 24
+    return (time.localtime()[3] + set_hour - hour_offset + (time.localtime()[4] + set_minute - minute_offset) // 60) % 24
     
 def minute():
     return (time.localtime()[4] + set_minute - minute_offset)%60   
