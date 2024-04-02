344e8d92b98ed9a5aa914bcb80b76cc4c0db13f1
m5preusser <124309201+m5preusser@users.noreply.github.com>
2024-04-02 17:39:13 -0600

    Update Logic.py

--- Logic.py
+++ Logic.py
@@ -11,7 +11,7 @@ def question_1():
     question = "Nice to talk to you again! Are you feeling well today?"
     # Name this recording "recording_1"
     Hardware.display_text(question, "middle")
-    Hardware.play_file("recording_1.wave")
+    Hardware.play_file("recording_1.wav")
     
     waiting = True
     response = ""
@@ -41,7 +41,7 @@ def question_2():
         # Name this recording "recording_2"
 
         Hardware.display_text(question, "middle")
-        Hardware.play_file("recording_2.wave")
+        Hardware.play_file("recording_2.wav")
 
         waiting = True
         response = ""
@@ -67,7 +67,7 @@ def question_2():
         # Name this recording "recording_3"
 
         Hardware.display_text(question, "middle")
-        Hardware.play_file("recording_3.wave")
+        Hardware.play_file("recording_3.wav")
 
         waiting = True
         response = ""
@@ -98,7 +98,7 @@ def question_3():
         # Name this recording "recording_4"
 
         Hardware.display_text(question, "middle")
-        Hardware.play_file("recording_4.wave")
+        Hardware.play_file("recording_4.wav")
 
         waiting = True
         response = ""
@@ -114,12 +114,12 @@ def question_3():
             terminating_statement = "What's the last thing to go through a flies head before it hits the fly swatter? Its bum. Thanks for speaking with me today!"
             # Name this recording "recording_5"
             Hardware.display_text(terminating_statement, "middle")
-            Hardware.play_file("recording_5.wave")
+            Hardware.play_file("recording_5.wav")
         elif response == "no":
             terminating_statement = "That's alright, one joke is enough for today. Thanks for the chat!"
             # Name this recording "recording_6"
             Hardware.display_text(terminating_statement, "middle")
-            Hardware.play_file("recording_6.wave")
+            Hardware.play_file("recording_6.wav")
         else:
             pass
     
@@ -127,7 +127,7 @@ def question_3():
         question = "That's alright, sometimes you're not in the mood for jokes. Would you like some current information?"
         # Name this recording "recording_7"
         Hardware.display_text(question, "middle")
-        Hardware.play_file("recording_7.wave")
+        Hardware.play_file("recording_7.wav")
 
         waiting = True
         response = ""
@@ -147,12 +147,12 @@ def question_3():
             # For this one, say "this is the current time and ambient temperature"
             # Name this recording "recording_8"
             Hardware.display_text(terminating_statement, "middle")
-            Hardware.play_file("recording_8.wave")
+            Hardware.play_file("recording_8.wav")
         elif response == "no":
             terminating_statement = "I hope I provided some emotional support today. It's always good to talk to someone."
             # Name this recording "recording_9"
             Hardware.display_text(terminating_statement, "middle")
-            Hardware.play_file("recording_9.wave")
+            Hardware.play_file("recording_9.wav")
         else:
             pass
 
@@ -160,7 +160,7 @@ def question_3():
         question = "Would you describe your pain as a headache?"
         # Name this recording "recording_10"
         Hardware.display_text(question, "middle")
-        Hardware.play_file("recording_10.wave")
+        Hardware.play_file("recording_10.wav")
 
         waiting = True
         response = ""
@@ -177,13 +177,13 @@ def question_3():
             terminating_statement = "The current ambient temperature is " + temperature + " °C. High temperatures can contribute to headahches, you may want to call a nurse to lower the temperature in your room."
             # Name this recording "recording_11"
             Hardware.display_text(terminating_statement, "middle")
-            Hardware.play_file("recording_11.wave")
+            Hardware.play_file("recording_11.wav")
 
         elif response == "no":
             terminating_statement = "If you're in any kind of significant anguish, I would highly reccomend calling a nurse. I am unfortunately limited in my ability to help physically."
             # Name this recording "recording_12"
             Hardware.display_text(terminating_statement, "middle")
-            Hardware.play_file("recording_12.wave")
+            Hardware.play_file("recording_12.wav")
         else:
             pass
 
@@ -191,7 +191,7 @@ def question_3():
         question = "That's good to hear at least. Are you in any emotional anguish?"
         # Name this recording "recording_13"
         Hardware.display_text(question, "middle")
-        Hardware.play_file("recording_13.wave")
+        Hardware.play_file("recording_13.wav")
 
         waiting = True
         response = ""
@@ -207,11 +207,11 @@ def question_3():
             terminating_statement = "Remember to drink and eat enough food and water throughout your day. That will help you keep your energy up!"
             # Name this recording "recording_14"
             Hardware.display_text(terminating_statement, "middle")
-            Hardware.play_file("recording_14.wave")
+            Hardware.play_file("recording_14.wav")
         elif response == "no":
             terminating_statement = "I won't be able to help you with much else. If you need something, I would reccomend calling a nurse. If not, try calling some family, social interaction can help your mental health."
             # Name this recording "recording_15"
             Hardware.display_text(terminating_statement, "middle")
-            Hardware.play_file("recording_15.wave")
+            Hardware.play_file("recording_15.wav")
         else:
             pass
